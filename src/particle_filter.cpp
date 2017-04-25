152220357cc78e0c0b0a9d4404061e8f88719478
AndrewGls <mrglushko@gmail.com>
2017-04-26 01:20:01 +0200

    feat: worked on ParticleFilter filter
    ParticleFilter::updateWeights() method is not finished yet.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -9,8 +9,14 @@
 #include <algorithm>
 #include <iostream>
 #include <numeric>
+#include <random>
+#include <limits>
+#include <assert.h>
 
+#include "helper_functions.h"
 #include "particle_filter.h"
+
+using namespace std;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// TODO: Set the number of particles. Initialize all particles to first position (based on estimates of 
@@ -18,6 +24,24 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 
+	particles.resize(num_particles);
+	weights.resize(num_particles, 1.);
+
+	default_random_engine gen;
+	normal_distribution<double> dist_x(x, std[0]);
+	normal_distribution<double> dist_y(y, std[1]);
+	normal_distribution<double> dist_theta(theta, std[2]);
+
+	for (int i = 0; i < num_particles; ++i) {
+		Particle& pa = particles[i];
+		pa.id = 0;
+		pa.x = dist_x(gen);
+		pa.y = dist_y(gen);
+		pa.theta = dist_theta(gen);
+		pa.weight = 1.;
+	}
+
+	is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
@@ -26,6 +50,31 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
 
+	const double epsilon = std::numeric_limits<double>::epsilon();
+
+	default_random_engine gen;
+	normal_distribution<double> N_x(0, std_pos[0]);
+	normal_distribution<double> N_y(0, std_pos[1]);
+	normal_distribution<double> N_yaw(0, std_pos[2]);
+
+	for (int i = 0; i < num_particles; ++i)
+	{
+		Particle& pa = particles[i];
+
+		if (FP_ZERO == fpclassify(yaw_rate)) {
+			// zero yaw rate
+			assert(false);
+			pa.x += velocity * delta_t * cos(pa.theta) + N_x(gen);
+			pa.y += velocity * delta_t * sin(pa.theta) + N_y(gen);
+		}
+		else {
+			// non-zero yaw rate
+			double curr_theta = pa.theta;
+			pa.theta += yaw_rate * delta_t + N_yaw(gen);
+			pa.x += velocity * (sin(pa.theta  ) - sin(curr_theta)) / yaw_rate + N_x(gen);
+			pa.y += velocity * (cos(curr_theta) - cos(pa.theta  )) / yaw_rate + N_y(gen);
+		}
+	}
 }
 
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
@@ -34,6 +83,61 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
 
+
+}
+
+namespace
+{
+	inline void TransfromObservationFromCarSpaceToMapSpace(const Particle& particle, LandmarkObs& observ)
+	{
+		// |x'| |cos(a) -sin(a) tx| |x|
+		// |y'|=|sin(a)  cos(a) ty|*|y|
+		// |1 | | 0        0     0| |1|
+
+		const double car_obs_x = observ.x;
+		const double car_obs_y = observ.y;
+		observ.x = cos(particle.theta) * car_obs_x - sin(particle.theta) * car_obs_y + particle.x;
+		observ.y = sin(particle.theta) * car_obs_x + cos(particle.theta) * car_obs_y + particle.y;
+	}
+
+	inline double CalcNormProb(const LandmarkObs& predicted, const LandmarkObs& observation, double sigma_x, double sigma_y)
+	{
+		double dx = observation.x - predicted.x;
+		double dy = observation.y - predicted.y;
+		return exp(-dx*dx / (2 * sigma_x*sigma_x) - dy*dy / (2 * sigma_y*sigma_y)) / (2 * M_PI * sigma_x * sigma_y);
+	}
+
+	void FindAssociation(std::vector<LandmarkObs>& predicted, const std::vector<LandmarkObs>& observations, const Map& map_landmarks)
+	{
+		predicted.resize(observations.size());
+
+		if (observations.empty())
+			return;
+
+		std::vector<int> min_dist(observations.size(), INT_MAX);
+		const vector<Map::single_landmark_s>& landmarks = map_landmarks.landmark_list;
+
+		for (int i = 0; i < landmarks.size(); i++)
+		{
+			const Map::single_landmark_s& lm = landmarks[i];
+
+			for (int j = 0; j < observations.size(); j++)
+			{
+				// find nearest landmark to observation
+				const LandmarkObs& obs = observations[j];
+				double distance = dist(lm.x_f, lm.y_f, obs.x, obs.y);
+				if (distance < min_dist[j])
+				{
+					min_dist[j] = distance;
+
+					LandmarkObs& pred = predicted[j];
+					pred.id = lm.id_i;
+					pred.x = lm.x_f;
+					pred.y = lm.y_f;
+				}
+			}
+		}
+	}
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
@@ -49,6 +153,27 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account 
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
+
+	std::vector<LandmarkObs> predicted;
+
+	for (int i = 0; i < num_particles; ++i)
+	{
+		Particle& pa = particles[i];
+
+		// transform observations from car space to map space using particle position and orientation.
+		TransfromObservationFromCarSpaceToMapSpace(pa, observations[i]);
+
+		// find nearest landmarks to observations
+		FindAssociation(predicted, observations, map_landmarks);
+
+		// update weight of particle using calculated Multivariate-Gaussian probability
+		double prob = 1.;
+		for (int i = 0; i < predicted.size(); i++) {
+			const LandmarkObs& pred = predicted[i];
+			prob *= CalcNormProb(predicted[i], observations[i], );
+		}
+		pa.weight *= prob;
+	}
 }
 
 void ParticleFilter::resample() {
